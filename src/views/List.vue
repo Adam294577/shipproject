69679b7ef559208d0168aac4e10b429b9bbad52c
Alex Chiu <alexchiu@datacom.com.tw>
2024-04-19 08:19:18 +0800

    Initial commit

---
+++ src/views/List.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="container-fluid bg-blue header">
+    <div class="row justify-content-center align-items-center h-100">
+      <div class="col-8">
+        <div class="header_avatar d-flex">
+          <div class="avatar position-relative">
+            <div>
+              <img
+                src="https://randomuser.me/api/portraits/men/75.jpg"
+                alt=""
+                class="img-fluid rounded-circle"
+              />
+            </div>
+            <p
+              class="text-light bg-orange fs-4 text-center position-absolute online-status"
+            >
+              在線中
+            </p>
+          </div>
+          <div class="avatar_detail ms-4 text-light">
+            <div class="name fw-bold mb-1">林美美</div>
+            <div class="med-status">
+              <p class="location mb-0 lh-sm">屏東縣琉球鄉衛生所</p>
+              <p class="title lh-sm">護理人員</p>
+              <div class="referral-status d-flex text-center fw-bold">
+                <div class="referral-ing">
+                  <p class="referraling-count fs-5 mb-0">36</p>
+                  <p>轉診中</p>
+                </div>
+                <div class="referral-finished ms-3">
+                  <p class="referral-finish-count fs-5 mb-0">124</p>
+                  <p>轉診完成</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid bg-lightblue list-content">
+    <div class="row justify-content-center">
+      <div class="col-8">sdfsaf</div>
+    </div>
+  </div>
+  <div class="container-fluid bg-blue footer">
+    <div class="row justify-content-center">
+      <div class="col-8">
+        <div class="bottom d-flex text-center text-light align-items-center">
+          <div class="search col">
+            <a href="#" class="text-light text-decoration-none">
+              <font-awesome-icon
+                :icon="['fas', 'magnifying-glass']"
+                style="color: #ffffff"
+                size="2xl"
+              />
+              <p class="mb-0">搜尋</p>
+            </a>
+          </div>
+          <div class="home col">
+            <a href="#" class="text-light text-decoration-none">
+              <font-awesome-icon
+                :icon="['fas', 'house']"
+                style="color: #ffffff"
+                size="2xl"
+              />
+              <p class="mb-0">首頁</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+
+/* add icons to the library */
+library.add(fab, fas, far);
+</script>
+
+<style>
+.container-fluid {
+  --bs-gutter-x: 0;
+}
+.bg-blue {
+  background-color: rgb(69, 115, 187);
+}
+.bg-lightblue {
+  background-color: rgb(236, 241, 248);
+}
+.header {
+  height: 300px;
+}
+/* .header_avatar {
+  background-color: red;
+  height: 100px;
+} */
+.bg-orange {
+  background-color: rgb(240, 163, 87);
+}
+.online-status {
+  top: 100px;
+  width: 100%;
+  border-radius: 10px;
+}
+.list-content {
+  height: 966px;
+}
+.footer {
+  height: 100px;
+  position: fixed;
+  bottom: 0;
+}
+.bottom {
+  height: 100px;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,119 @@
+<script setup>
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+
+/* add icons to the library */
+library.add(fab, fas, far);
+
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+function loginHandle(event) {
+  console.log("login");
+  router.push("/list");
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="row justify-content-center mb-5">
+      <div class="col">
+        <img
+          src="/src/assets/images/ambulanceLogo.svg"
+          alt=""
+          class="img-fluid d-block mx-auto"
+        />
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="input-group input-group-lg mb-3">
+        <input
+          type="text"
+          class="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-lg"
+          placeholder="帳號"
+        />
+      </div>
+      <div class="input-group input-group-lg">
+        <input
+          type="password"
+          class="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-lg"
+          placeholder="密碼"
+        />
+      </div>
+      <a href="#" class="d-block text-end text-dark text-decoration-none pe-3 my-2"
+        ><small>忘記密碼</small></a
+      >
+      <div class="col">
+        <div class="input-group input-group-lg mb-3">
+          <input
+            type="text"
+            class="form-control"
+            aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-lg"
+            placeholder="驗證碼"
+          />
+        </div>
+      </div>
+      <div class="col valid-code d-flex align-items-center justify-content-between px-4">
+        <p class="text-light fs-5 mb-0">ba762q</p>
+        <a href="#"
+          ><font-awesome-icon :icon="['fas', 'arrows-rotate']" style="color: #ffffff"
+        /></a>
+      </div>
+      <div class="form-check form-switch">
+        <input
+          class="form-check-input fs-4"
+          type="checkbox"
+          role="switch"
+          id="flexSwitchCheckDefault"
+        />
+        <label class="form-check-label fs-4" for="flexSwitchCheckDefault">記住帳號</label>
+      </div>
+      <div class="d-grid gap-2 mt-3">
+        <button class="btn py-2" type="button" @click="loginHandle">登入</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin-top: 400px;
+  padding: 0 200px;
+}
+::placeholder {
+  color: gray;
+  opacity: 0.5; /* Firefox */
+}
+
+::-ms-input-placeholder {
+  /* Edge 12-18 */
+  color: gray;
+}
+
+.valid-code {
+  background: rgb(38, 56, 88);
+  height: 48px;
+  margin-right: 13px;
+  border-radius: 5px;
+}
+
+.form-switch {
+  padding-left: 4.5em;
+}
+.btn {
+  background-color: rgb(240, 179, 115);
+}
+</style>
